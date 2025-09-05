44efa1fd070a2836c719906f0f6bbbf940abc0c7
dhoat30 <dhoat30@gmail.com>
2025-09-05 18:29:55 +1200

    added redirection

--- next.config.js
+++ next.config.js
@@ -81,6 +81,16 @@ const nextConfig = {
                 destination: '/paihia/indian/menu',
                 permanent: true,
             },
+            {
+                source: '/paihia/indian',
+                destination: '/paihia/thai',
+                permanent: true,
+            },
+            {
+                source: '/russell/indian',
+                destination: '/russell/thai',
+                permanent: true,
+            },
         ]
     }
 }
